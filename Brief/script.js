7f823c752f70c6dadad19a4c8afb86153a8575d0
majmehdi <majmehdi20@gmail.com>
2024-10-29 17:24:22 +0100

    add Javascript

---
+++ Brief/script.js
@@ -0,0 +1,15 @@
+const inputBox = document.getElementById("inputBox")
+
+const list = document.getElementById("list")
+
+function addTask() {
+
+
+    if(inputBox.value === ''){
+        alert("You must write something");
+    }
+
+    // else{
+    //     let 
+    // }
+}
